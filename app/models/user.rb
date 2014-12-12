137fd7eb8e7ec49a1725a283940a7676727a99d1
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-12 10:41:06 -0500

    added associations and validations to all models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
+  has_many :projects, through: :tasks
+  has_many :tasks
+
+  validates :email, presence: true, uniqueness: true, format: {with: /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b?/i}
+
+  validates :password, presence: true
+
 
 end
