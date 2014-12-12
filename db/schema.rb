8344a202f0f78f9d578109de56e74c019daeeb5b
William Mahoney <wm.j.mahoney@gmail.com>
2014-12-12 10:49:26 -0500

    added timestamps to tasks table to keep tables consistent with these columns

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141212144841) do
+ActiveRecord::Schema.define(version: 20141212154231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20141212144841) do
     t.datetime "due_date"
     t.integer  "user_id"
     t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
